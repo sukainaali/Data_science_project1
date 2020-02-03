2787df77bd1e07cfd19633f84193186c55bc86c2
Sukaina Ali <koona.ali@gmail.com>
2020-02-03 15:45:19 -0600

    done till problem 3

--- prediction_project.py
+++ prediction_project.py
@@ -1,1 +1,6 @@
-
+import pandas as pd
+ship= pd.read_csv('cruise_ship_info.csv')
+ship_columns= ship.head()
+print(ship_columns)
+ship_stat= ship.describe()
+print(ship_stat)
--- project1.py
+++ project1.py
@@ -1,1 +1,4 @@
-print('hello')
+import pandas as import pd
+ship_crew= pd.read_csv("cruise_ship_info.csv")
+ship_crew_columns= ship_crew.columns
+print(ship_crew_columns)
